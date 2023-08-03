9455f44bb6d94e715948e6341ef481de5d4c2c62
Dronine <99697658+DronineOnlyOne@users.noreply.github.com>
2023-08-03 20:05:36 -0300

    activities

---
+++ ConsoleAppCM/ConsoleAppCM/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleAppCM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string and press enter: "); // receive the value inserted
+            string readInput = Console.ReadLine(); // read the write line in console
+            Console.WriteLine("You have {0}", readInput); // return the typed value
+
+            Console.Write("Enter any key of your keyboard and press enter: ");
+            int asciiValue = Console.Read(); // read and return the value of key in console
+            Console.WriteLine("ASCII value is {0}", asciiValue);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleAppDtp/ConsoleAppDtp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleAppDtp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaring multiple variables at once
+            int num3, num4, num5;
+            // declaring a variable
+            int num1;
+            // assigning a value to a varibale
+            num1 = 13;
+
+            // declaring an initializing a variable in one go
+            int num2 = 13;
+            int sum = num1 + num2;
+
+            // using concatination
+            Console.WriteLine("num1 " + num1 + "+ num2 " + num2 + " is: " + sum);
+
+            Console.WriteLine("-----------//------------");
+
+            // using double datatype
+            double d1 = 3.1415;
+            double d2 = 5.1;
+            double dDiv = d1/ d2;
+            Console.WriteLine("d1 / d1 is " + dDiv);
+
+            Console.WriteLine("-----------//------------");
+
+            // using float datatype
+            float f1 = 3.1415f;
+            float f2 = 5.1f;
+            float fDiv = f1 / f2;
+            Console.WriteLine("f1 / f2 is " + fDiv);    // float is less precise than double. But, float uses less memory than double.
+
+            Console.WriteLine("-----------//------------");
+
+
+            Console.Read(); // used for not close the console
+        }
+    }
+}
