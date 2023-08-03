f85605bc1af82239058f9d34cd7800c5792f8129
Dronine <matheu.radical@hotmail.com>
2023-08-03 19:51:03 -0300

    Adicionar arquivos de projeto.

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SwitchStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+
+            switch(age)
+            {
+                case 15: // this case compared with varibale age
+                    Console.WriteLine("Too yong to party in the club!"); // if age < case 15, this CW starts
+                    break; // i need write the "break" for the switch works. Because, if this case to be true, the break stops
+                case 25:
+                    Console.WriteLine("Good to go!");
+                    break;
+                default: // is called every the others is true
+                    Console.WriteLine("how old are you then?"); // works like else for the if else statements
+                    break;
+
+            }
+        }
+    }
+}
